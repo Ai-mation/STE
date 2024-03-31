4eda05db273ac176c2696ed8f858a8bcac334ffb
Ayman Alhandali <aymanalhandali@Aymans-Air.lan>
2024-03-30 23:15:03 -0700

    merging

--- src/ste/api.py
+++ src/ste/api.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
-from inst import Experiement
+from src.ste.inst import Experiement
 
 app = FastAPI()
 
 
 exp = Experiement()
-
 
 
 @app.get("/station")
@@ -16,7 +15,7 @@ async def get_station():
 
 @app.post("/create_instrument/{inst_name}")
 async def add_parameter(inst_name: str):
-    exp.create_instrument(name = inst_name)
+    exp.create_instrument(name=inst_name)
     ste = exp.show_station()
     return ste
 
--- src/ste/inst.py
+++ src/ste/inst.py
@@ -2,18 +2,14 @@ from qcodes.instrument_drivers.mock_instruments import DummyInstrument
 from qcodes.utils import validators as vals
 from qcodes.station import Station
 
+
 class Experiement:
     def __init__(self) -> None:
         self.ste = Station()
-
 
     def create_instrument(self, name: str):
         _inst = DummyInstrument(name=name)
         self.ste.add_component(_inst, name)
 
-
     def show_station(self):
         return self.ste.snapshot()
-    
-
-    

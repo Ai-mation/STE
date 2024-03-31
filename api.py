6e45a1dd7888c6026bca1c6cbce76b108eb547f7
Ayman Alhandali <aymanalhandali@Aymans-Air.lan>
2024-03-30 22:23:32 -0700

    first commit

---
+++ api.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from inst import Experiement
+
+app = FastAPI()
+
+
+exp = Experiement()
+
+
+
+@app.get("/station")
+async def get_station():
+    ste = exp.show_station()
+    return ste
+
+
+@app.post("/create_instrument/{inst_name}")
+async def add_parameter(inst_name: str):
+    exp.create_instrument(name = inst_name)
+    ste = exp.show_station()
+    return ste
+
+
+# Run the application
+# uvicorn main:app --reload
